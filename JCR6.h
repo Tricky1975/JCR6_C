6d4671de41dfe9ecf7264caff32f446506cdfb33
Tricky1975 <facebook@tbbs.nl>
2018-10-30 17:53:36 +0100

    ZLib license

--- JCR6.h
+++ JCR6.h
@@ -1,3 +1,22 @@
+// Lic:
+//   JCR6.h
+//   JCR6 for C
+//   version: 18.10.30
+//   Copyright (C) 2018 Header file for JCR6 library for C
+//   This software is provided 'as-is', without any express or implied
+//   warranty.  In no event will the authors be held liable for any damages
+//   arising from the use of this software.
+//   Permission is granted to anyone to use this software for any purpose,
+//   including commercial applications, and to alter it and redistribute it
+//   freely, subject to the following restrictions:
+//   1. The origin of this software must not be misrepresented; you must not
+//      claim that you wrote the original software. If you use this software
+//      in a product, an acknowledgment in the product documentation would be
+//      appreciated but is not required.
+//   2. Altered source versions must be plainly marked as such, and must not be
+//      misrepresented as being the original software.
+//   3. This notice may not be removed or altered from any source distribution.
+// EndLic
 #ifndef __JEROENS_COLLECTED_RESOURCE_VERSION_6_LOADED__
 #define __JEROENS_COLLECTED_RESOURCE_VERSION_6_LOADED__
 
